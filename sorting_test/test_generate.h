2991d345096b84820c2798ec4e7ef25a8ad90616
Chivier <chivier.humber@gmail.com>
2019-09-05 19:32:57 +0800

    Add comments for Class define.

--- sorting_test/test_generate.h
+++ sorting_test/test_generate.h
@@ -12,13 +12,16 @@ private:
     std::vector<int> number;
 
 public:
+    // * Initailize the class.
     Test_case() {
         test_size = 0;
         number.clear();
     }
 
+    // * Update new size for a certain test case.
     void update_size(int new_size) { test_size = new_size; }
 
+    // * Generate new random data.
     void generate_random() {
         srand(time(NULL));
         number.clear();
@@ -26,9 +29,11 @@ public:
             number.push_back(rand());
     }
 
+    // * Return all items.
     std::vector<int> return_case() { return number; }
     int return_size() { return test_size; }
 
+    // * Put out numbers in one case.
     void putout() {
         for (auto i = number.begin(); i < number.end(); ++i) {
             printf("%15d", (*i));

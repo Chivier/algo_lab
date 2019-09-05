932515e1b1a1087c98bdfa14c6324aece8f448f7
Chivier <chivier.humber@gmail.com>
2019-09-05 19:35:38 +0800

    Correct the spell errors.

--- sorting_test/my_sorting.h
+++ sorting_test/my_sorting.h
@@ -8,8 +8,11 @@ typedef std::vector<int> Arr;
 
 int bubble_sort(Arr &origin);
 int bubble_sort_paralled(Arr &origin);
+
 int insert_sort(Arr &origin);
+
 int merge_sort(Arr &origin);
 int merge_sort_paralled(Arr &origin);
+
 int quick_sort(Arr &origin, int lb, int rb);
 int quick_sort_parralled(Arr &origin, int lb, int rb);

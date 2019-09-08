9aaf1c553e202d19412dec6f8b1c506e0c8155fb
Chivier <chivier.humber@gmail.com>
2019-09-08 15:36:20 +0800

    Basic Insertion sort is finished.

--- sorting_test/my_sorting.cpp
+++ sorting_test/my_sorting.cpp
@@ -4,3 +4,20 @@ int bubble_sort(Arr &origin) {
     int i;
     return 1;
 }
+
+int insertion_sort(Arr &origin) {
+    if (origin.size() == 0)
+        return 0;
+    for (auto i = origin.begin() + 1; i < origin.end(); ++i) {
+        int key = *i;
+        for (auto j = i - 1; j >= origin.begin(); --j) {
+            if (*j > key)
+                *(j + 1) = *j;
+            else {
+                *j = key;
+                break;
+            }
+        }
+    }
+    return 1;
+}

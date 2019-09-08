75422c547b8e49a9278d37c33066b7f075d48cf4
Chivier <chivier.humber@gmail.com>
2019-09-08 15:28:07 +0800

    None

--- sorting_test/my_sorting.cpp
+++ sorting_test/my_sorting.cpp
@@ -1,6 +1,9 @@
 #include "my_sorting.h"
 
 int bubble_sort(Arr &origin) {
-    int i;
+    bool flag = 1;
+    for (int i = 0; i < origin.size(); ++i) {
+        for (int j = 0; j < origin.size())
+    }
     return 1;
 }

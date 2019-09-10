55ba5b035447c938c798b0cf51901ef56a2701c1
Chivier <chivier.humber@gmail.com>
2019-09-10 14:17:46 +0800

    test

--- sorting_test/my_sorting.cpp
+++ sorting_test/my_sorting.cpp
@@ -43,13 +43,49 @@ int insertion_sort(Arr &origin) {
         ++j;
         *j = key;
 
-        for (auto test = origin.begin(); test < origin.end(); ++test)
-            std::cout << *test << " ";
-        std::cout << "\n";
+        // for (auto test = origin.begin(); test < origin.end(); ++test)
+        //     std::cout << *test << " ";
+        // std::cout << "\n";
     }
     return 1;
 }
 
 int bubble_sort_paralled(Arr &origin) {
+    bool flag = 1;
+
+    int tag = 0;
+    omp_set_num_threads(8);
+#pragma omp parallel for num_threads(8)
+    for (int i = 1; i <= 100; i++) {
+        tag += i;
+        std::cout << i << " : " << omp_get_thread_num() << "   ";
+        std::cout << omp_get_num_threads() << "   ";
+        std::cout << omp_in_parallel() << "\n";
+    }
+    std::cout << tag << std::endl;
+    std::cout << tag << std::endl;
+    flag = 0;
+
+    while (flag) {
+        flag = 0;
+#pragma omp parallel for num_threads(8)
+        for (auto i = origin.begin(); i < origin.end(); i += 2) {
+            if (*(i) > *(i + 1)) {
+                std::swap(*i, *(i + 1));
+                flag = 1;
+            }
+            // std::cout << omp_get_thread_num() << "   ";
+            // std::cout << omp_get_num_threads() << "   ";
+            // std::cout << omp_in_parallel() << "\n";
+        }
+#pragma ivdep
+#pragma omp parallel for shared(flag) private(i)
+        for (auto i = origin.begin() + 1; i < origin.end(); i += 2) {
+            if (*(i) > *(i + 1)) {
+                std::swap(*i, *(i + 1));
+                flag = 1;
+            }
+        }
+    }
     return 1;
 }
--- sorting_test/sorting.cpp
+++ sorting_test/sorting.cpp
@@ -1,4 +1,5 @@
 #include "my_sorting.h"
+#include "omp.h"
 #include "test_generate.h"
 
 Test_case test;
@@ -6,17 +7,35 @@ Test_case test;
 int main() {
     Generate_small_case(test);
     test.putout_small();
-
+    //
+    //* Test insertion sort.
     std::vector<int> data = test.return_case();
     insertion_sort(data);
     for (auto i = data.begin(); i < data.end(); ++i)
         std::cout << *i << " ";
     std::cout << std::endl;
 
+    // * Test bubble sort.
     data = test.return_case();
     bubble_sort(data);
-
     for (auto i = data.begin(); i < data.end(); ++i)
         std::cout << *i << " ";
     std::cout << std::endl;
+
+    // * Test para-bubble sort.
+    data = test.return_case();
+    bubble_sort_paralled(data);
+    for (auto i = data.begin(); i < data.end(); ++i)
+        std::cout << *i << " ";
+    std::cout << std::endl;
+
+#pragma omp parallel
+    {
+        printf("The parallel region is executed by thread %d\n",
+               omp_get_thread_num());
+        if (omp_get_thread_num() == 2) {
+            printf(" Thread %d does things differently\n",
+                   omp_get_thread_num());
+        }
+    } /*-- End of parallel region --*/
 }
